63ca3ac94d06784ab0b45bd4f0b234b8b2b255f6
sebasdurango <sebasdurango19@gmail.com>
2024-07-27 16:40:31 -0500

    configure Redux

---
+++ src/components/Counter.jsx
@@ -0,0 +1,9 @@
+
+export const Counter = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
---
+++ src/redux/slice.js
@@ -0,0 +1,33 @@
+import { createSlice} from '@reduxjs/toolkit';
+
+//Definir el Slice para el componente Counter
+const counterSlice = createSlice({
+    name: 'counter',
+    initialState: 0,
+    reducers: {
+        //Definir cual es el reducer para incrementar el contador   
+        increment: state => state + 1,
+        //Definir cual es el reducer para decrementar el contador   
+        decrement: state => state - 1
+        
+    }
+});
+
+//Definir eñ Slice para el componente User
+const userSlice = createSlice({
+    name: 'user',
+    initialState: {name: ''},
+    reducers: {
+        setName: (state, action) => {
+            return { ...state, name: action.payload };
+        }
+    }
+});
+
+//Exportar las acciones de los slices
+export const {increment, decrement} = counterSlice.actions;
+export const { setName } = counterSlice.actions;
+
+//Exportar los reducers
+export const counterReducer = counterSlice.reducer;
+export const userReducer = userSlice.reducer;
